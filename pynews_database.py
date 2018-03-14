52f0119d36314b41d125894d136be3e2dd60bb4e
zzaakiirr <zzaakiirr@gmail.com>
2018-03-14 17:29:16 +0300

    Refactored code
    Rewrited old code by using functions, everything works the same

--- pynews_database.py
+++ pynews_database.py
@@ -1,8 +1,8 @@
 import os
 import json
 import vk
+import helpers
 
-new_pynews_list = []
 
 vk_app_id = os.environ.get('vk_app_id')
 vk_login = os.environ.get('vk_login')
@@ -13,11 +13,26 @@ session = vk.AuthSession(
     user_password=vk_password)
 api = vk.API(session, v=5.73, lang='en')
 
-try:
-    open('pynews_database.json')
-except FileNotFoundError:
+
+def fetch_news_from_vk():
+    try:
+        open('pynews_database.json')
+    except FileNotFoundError:
+        new_pynews_list = create_new_pynews_database()
+    else:
+        new_pynews_list = update_current_news_database()
+
+    return new_pynews_list
+
+
+def create_new_pynews_database():
+    new_pynews_list = []
     new_pynews_list.append(api.newsfeed.search(q='Python language'))
-else:
+
+    return new_pynews_list
+
+
+def update_current_news_database():
     with open('pynews_database.json') as f_obj:
         old_pynews_database = json.loads(f_obj.read())
         new_pynews_dict = api.newsfeed.search(
@@ -26,5 +41,10 @@ else:
         new_pynews_list = old_pynews_database
         new_pynews_list.append(new_pynews_dict)
 
-with open('pynews_database.json', 'w') as outfile:
-    json.dump(new_pynews_list, outfile)
+    return new_pynews_list
+
+
+if __name__ == '__main__':
+    new_pynews_list = fetch_news_from_vk()
+    with open('pynews_database.json', 'w') as outfile:
+        json.dump(new_pynews_list, outfile)
