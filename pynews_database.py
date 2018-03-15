29df5c050d068c0cf65ecaeff5bbb601ba6961a7
zzaakiirr <zzaakiirr@gmail.com>
2018-03-15 13:52:32 +0300

    Beautidied old functions (variables names) & added json_loading function

--- pynews_database.py
+++ pynews_database.py
@@ -13,40 +13,44 @@ session = vk.AuthSession(
 api = vk.API(session, v=5.73, lang='en')
 
 
-def fetch_news_from_vk():
+def dump_new_pynews_to_database(new_pynews):
+    with open('pynews_database.json', 'w') as outfile:
+        json.dump(new_pynews, outfile)
+
+
+def fetch_pynews_from_vk():
     try:
         open('pynews_database.json')
     except FileNotFoundError:
-        new_pynews_list = create_new_pynews_database()
+        new_pynews = create_new_pynews_database()
     else:
-        new_pynews_list = update_current_news_database()
+        new_pynews = update_current_pynews_database()
 
-    return new_pynews_list
+    return new_pynews
 
 
 def create_new_pynews_database():
-    new_pynews_list = []
-    new_pynews_list.append(api.newsfeed.search(q='Python language'))
+    new_pynews = []
+    new_pynews.append(api.newsfeed.search(q='Python language'))
 
-    return new_pynews_list
+    return new_pynews
 
 
-def update_current_news_database():
-
+def update_current_pynews_database():
     with open('pynews_database.json') as f_obj:
         old_pynews_database = json.loads(f_obj.read())
-        last_added_news_dict = old_pynews_database[-1]
-        page_for_searching = last_added_news_dict['next_from']
-        new_pynews_dict = api.newsfeed.search(
-            q='Python language',
-            start_from=page_for_searching)
-        new_pynews_list = old_pynews_database
-        new_pynews_list.append(new_pynews_dict)
 
-    return new_pynews_list
+    last_added_news_to_old_pynews_database = old_pynews_database[-1]
+    page_for_searching = last_added_news_to_old_pynews_database['next_from']
+    new_pynews_dict = api.newsfeed.search(
+        q='Python language',
+        start_from=page_for_searching)
+    new_pynews = old_pynews_database
+    new_pynews.append(new_pynews_dict)
+
+    return new_pynews
 
 
 if __name__ == '__main__':
-    new_pynews = fetch_news_from_vk()
-    with open('pynews_database.json', 'w') as outfile:
-        json.dump(new_pynews, outfile)
+    new_pynews = fetch_pynews_from_vk()
+    dump_new_pynews_to_database(new_pynews)
