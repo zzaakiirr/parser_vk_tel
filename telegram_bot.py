2901e4e25fea4444e9d30450c5761d1587a127f6
zzaakiirr <zzaakiirr@gmail.com>
2018-03-15 13:56:56 +0300

    Deleted from telegram_bot echo function & added command "/pynews"
    new function /pynews uses vk_post_helpers & pynews_database modules,
    also it updates pynews and upload this to database

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,11 +1,10 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 import os
+import pynews_database
+import vk_post_helpers
 
 
-telegram_bot_access_token = os.environ.get('telegram_bot_access_token')
-
-# Enable logging
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO)
@@ -13,21 +12,12 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-# Define a few command handlers. These usually take the two arguments bot and
-# update. Error handlers also receive the raised TelegramError object in error.
 def start(bot, update):
     """Send a message when the command /start is issued."""
-    update.message.reply_text("Hello, I'm a parser. What can I do?\nI find news about Python (programming language) in social site http://vk.com and send them to you here!\nJust put down '/pynews'")
-
-
-def help(bot, update):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
-
-
-def echo(bot, update):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
+    update.message.reply_text(
+        "Hello, I'm a parser. What can I do?\nI find news about Python " +
+        "(programming language) in social site http://vk.com " +
+        "and send them to you here!\nJust put down '/pynews'")
 
 
 def error(bot, update, error):
@@ -35,32 +25,28 @@ def error(bot, update, error):
     logger.warning('Update "%s" caused error "%s"', update, error)
 
 
+def pynews(bot, update):
+    new_pynews = pynews_database.fetch_pynews_from_vk()
+    random_post = vk_post_helpers.get_random_post_from_database(
+        new_pynews)
+    post_link = vk_post_helpers.create_post_link(random_post)
+    pynews_database.dump_new_pynews_to_database(new_pynews)
+    update.message.reply_text(post_link)
+
+
 def main():
     """Start the bot."""
-    # Create the EventHandler and pass it your bot's token.
+    telegram_bot_access_token = os.environ.get('telegram_bot_access_token')
     updater = Updater(telegram_bot_access_token)
 
-    # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
-    # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
-
-    # log all errors
+    dp.add_handler(CommandHandler("pynews", pynews))
     dp.add_error_handler(error)
 
-    # Start the Bot
     updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
-
 
 if __name__ == '__main__':
     main()
